b7a6c65b624db6587a0dde226a762e6ac2886bc7
Stefan Zlobin <stefan1515@mail.ru>
2021-01-24 09:46:21 +0300

    Тестовая страница "Подробнее о курсе"

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -63,52 +63,31 @@ $(function () {
       }
    });
 });
-let moo = document.querySelectorAll('.courses-detailed__row');
+let moo = document.querySelectorAll('.program__row-content-title');
 let active = moo[0]; // moo[0], чтобы при первом клике не было ошибки.
 
 for (let i = 0; i < moo.length; i++) {
    moo[i].addEventListener('click', function () {
       if (active == this) {
          // Если active ссылается на тот же объект, что и this (на кликнутую кнопку)
-         this.classList.toggle('courses-detailed__active');
+         this.classList.toggle('active');
       } else {
-         active.classList.remove('courses-detailed__active');
-         this.classList.add('courses-detailed__active');
+         active.classList.remove('active');
+         this.classList.add('active');
 
          active = this;
       }
    });
 }
 
-$('.courses-detailed__column-none').hide();
-
-$('.courses-detailed__row1').click(function () {
-   $('.courses-detailed__column1').show();
-   $('.courses-detailed__column2').hide();
-   $('.courses-detailed__column3').hide();
-   $('.courses-detailed__column4').hide();
-});
-
-$('.courses-detailed__row2').click(function () {
-   $('.courses-detailed__column2').show();
-   $('.courses-detailed__column1').hide();
-   $('.courses-detailed__column3').hide();
-   $('.courses-detailed__column4').hide();
-});
-
-$('.courses-detailed__row3').click(function () {
-   $('.courses-detailed__column3').show();
-   $('.courses-detailed__column2').hide();
-   $('.courses-detailed__column1').hide();
-   $('.courses-detailed__column4').hide();
-});
-
-$('.courses-detailed__row4').click(function () {
-   $('.courses-detailed__column4').show();
-   $('.courses-detailed__column1').hide();
-   $('.courses-detailed__column3').hide();
-   $('.courses-detailed__column2').hide();
-});
+$(function () {
+   $('.program__row-content-show-by-click').hide();
+   $('.display-click1').show();
+   $('.program__row-content-title').click(function () {
+      $('.program__row-content-show-by-click').hide();
+      $('.display-click' + $(this).attr('target')).show();
+   })
+})
 let check = 1;
 let target = $('.experience__number');
 let targetPos = target.offset().top;
